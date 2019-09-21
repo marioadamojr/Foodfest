e78c78f7995fcb5233034a137fbefd15a3838112
marioadamojr <mario.adamo.jr@gmail.com>
2019-09-21 12:18:06 -0400

    set up truck

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   resources :guests
 
-  # resources :trucks
+  resources :trucks
   
   # resources :foods
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
